a2c79b5e18d4a4064ec69f075d33a0cd403c9bd6
pbkabali <engineerbpk@gmail.com>
2023-10-16 03:13:17 +0300

    setup project

---
+++ app/LiveResults/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LiveResults = () => {
+  return (
+    <div>LiveResults</div>
+  )
+}
+
+export default LiveResults
---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Link from "next/link"
+
+export default function Home() {
+  return (
+    <main>
+    <h1>Welcome</h1>
+    <Link href='/LiveResults'>Live Results</Link>
+    </main>
+  )
+}
